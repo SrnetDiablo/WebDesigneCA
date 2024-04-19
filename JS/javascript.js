ac17609de32a6a22b905486c8c2d3ad2b8895e76
Koya Funaki <163404440+koyaFuanki@users.noreply.github.com>
2024-04-20 00:14:50 +0100

    Add files via upload

--- JS/javascript.js
+++ JS/javascript.js
@@ -1,8 +1,81 @@
+//option for bxslider
 $(document).ready(function () {
       $('#bxslider').bxSlider({
         auto: true,
         pager: false,
         speed: 700,
+        adaptiveHeight: true
     });
 });
 
+
+
+document.addEventListener("DOMContentLoaded", function() {
+    var image = document.getElementById("image");
+    var textContainer = document.getElementById("text-container");
+
+    // When you click
+    image.addEventListener("click", function() {
+       // check if shoews text
+        if (textContainer.style.display === "none") {
+            textContainer.style.display = "block"; // display text
+        } else {
+            textContainer.style.display = "none"; // hide text
+        }
+    });
+});
+
+document.addEventListener("DOMContentLoaded", function() {
+    var image = document.getElementById("image2");
+    var textContainer = document.getElementById("text-container2");
+
+    // When you click 
+    image.addEventListener("click", function() {
+        // check if shoews text
+        if (textContainer.style.display === "none") {
+            textContainer.style.display = "block"; // display text
+        } else {
+            textContainer.style.display = "none"; // hide text
+        }
+    });
+});
+
+document.addEventListener("DOMContentLoaded", function() {
+    var image = document.getElementById("image3");
+    var textContainer = document.getElementById("text-container3");
+
+    // When you click
+    image.addEventListener("click", function() {
+       // check if shoews text
+        if (textContainer.style.display === "none") {
+            textContainer.style.display = "block"; // display text
+        } else {
+            textContainer.style.display = "none"; // hide text
+        }
+    });
+});
+
+document.addEventListener("DOMContentLoaded", function() {
+    var image = document.getElementById("image4");
+    var textContainer = document.getElementById("text-container4");
+
+    // When you click
+    image.addEventListener("click", function() {
+       // check if shoews text
+        if (textContainer.style.display === "none") {
+            textContainer.style.display = "block"; // display text
+        } else {
+            textContainer.style.display = "none"; // hide text
+        }
+    });
+});
+
+//bxslider adjastment of height
+$(document).ready(function(){
+    $('#bxslider').bxSlider({
+        mode: 'fade',
+        captions: true,
+        auto: true,
+        speed: 700
+    });
+});
